b5f386bc953d3012c7463db5db022f1724eec4a3
Kevin Conley <kevindconley@gmail.com>
2018-03-14 17:00:42 -0700

    Implement "get ready to look around" timed scene

---
+++ Paint-mate/Assets/Scripts/TimedSwitchScenes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using HoloToolkit.Examples.Prototyping;
+
+public class TimedSwitchScenes : MonoBehaviour {
+
+	public float delay;
+
+	// Use this for initialization
+	void Start()
+	{
+		StartCoroutine(LoadSceneAfterDelay());     
+	}
+
+	IEnumerator LoadSceneAfterDelay()
+	{
+		yield return new WaitForSeconds(this.delay);
+		SceneSwitcher switcher = new SceneSwitcher();
+		switcher.NextScene();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
